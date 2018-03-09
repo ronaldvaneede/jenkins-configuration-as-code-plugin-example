fc817e81d333007e1151a91a1268c93273d95f98
Ronald van Eede <ronaldvaneede@gmail.com>
2018-03-09 16:23:00 +0100

    Using seperate git repos for the example projects

--- jenkins/projects.groovy
+++ jenkins/projects.groovy
@@ -5,8 +5,8 @@ folder(basePath) {
 }
 
 def projects = [
-        [ name: 'project1', repo: 'https://github.com/rveede/jenkins-configuration-as-code-plugin-example.git'],
-        [ name: 'project2', repo: 'https://github.com/rveede/jenkins-configuration-as-code-plugin-example.git']
+    [ name: 'project1', repo: 'git@github.com:rveede/example-project-1.git'],
+    [ name: 'project2', repo: 'git@github.com:rveede/example-project-2.git']
 ]
 
 projects.each { project ->
@@ -14,14 +14,6 @@ projects.each { project ->
         branchSources {
             git {
                 remote("${project.repo}")
-            }
-        }
-        configure {
-            // This is a hack because the JobDSL does not yet have a nice way to define the scriptPath afaik
-            // This is needed because the Jenkinksfile is not in the root...this happens when you do not stick to the convention
-            it / factory(class: 'org.jenkinsci.plugins.workflow.multibranch.WorkflowBranchProjectFactory') {
-                owner(class: 'org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject', reference: '../..')
-                scriptPath("${project.name}/Jenkinsfile")
             }
         }
         triggers {
